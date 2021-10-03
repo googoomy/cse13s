6bcb02f93a2a3e902f8e650893e339028a532e02
James Gu <jjgu@ucsc.edu>
2021-10-03 21:35:51 +0000

    added comments

--- asgn1/pig.c
+++ asgn1/pig.c
@@ -11,7 +11,7 @@
 int main(void) {
     typedef enum { SIDE, RAZORBACK, TROTTER, SNOUTER, JOWLER } Position;
     const Position pig[7] = { SIDE, SIDE, RAZORBACK, TROTTER, SNOUTER, JOWLER, JOWLER };
-
+    //Ask the user for how many players in the game. If the number is not allowed the default number of players is 2
     int num_Players = 0;
     printf("How many players? ");
     scanf("%d", &num_Players);
@@ -19,7 +19,7 @@ int main(void) {
         num_Players = MIN_PLAYERS;
         fprintf(stderr, "Invalid number of players. Using %d instead.\n", num_Players);
     }
-
+    //Ask the user the seed the game will be played under. If the number is a negative integer the default seed is 2021
     int user_Seed = 0;
     printf("Random seed: ");
     scanf("%d", &user_Seed);
@@ -27,11 +27,13 @@ int main(void) {
         user_Seed = SEED;
         fprintf(stderr, "Invalid random seed. Using %d instead.\n", user_Seed);
     }
-
+    //This is where the main implementation of the game is
+    //points are stored in an array so the players' scores are mapped to the same position in both arrays
     int points[MAX_PLAYERS] = { 0 };
     srandom(user_Seed);
-    int current_Player = 0;
-    int last_Player;
+    int current_Player = 0; //this variable keeps track of the current player
+    int last_Player; //this variable keeps track of the last player so the rolls the pig line doesn't repeat every loop
+    //the game ends when a player reaches 100 points or more
     while (points[current_Player] < MAX_POINTS) {
         Position roll;
         roll = pig[random() % 7];
@@ -40,36 +42,39 @@ int main(void) {
         }
         last_Player = current_Player;
         switch (roll) {
+        //if the player rolls SIDE the turn is passed over to the next player
         case SIDE:
             printf(" pig lands on side");
             current_Player += 1;
+            //if the last player rolls side the next player will be the first player. It loops around.
             if (current_Player > num_Players - 1) {
                 current_Player = 0;
             }
             printf("\n");
             break;
+        //if the player rolls RAZORBACK they gain 10 points
         case RAZORBACK:
             printf(" pig lands on back");
             points[current_Player] += 10;
-            //last_Player = current_Player;
             break;
+        //if the player rolls TROTTER they gain 10 points
         case TROTTER:
             printf(" pig lands upright");
             points[current_Player] += 10;
-            //last_Player = current_Player;
             break;
+        //if the player rolls SNOUTER they gain 15 points
         case SNOUTER:
             printf(" pig lands on snout");
             points[current_Player] += 15;
-            //last_Player = current_Player;
             break;
+        //if the player rolls JOWLER they gain 5 points
         case JOWLER:
             printf(" pig lands on ear");
             points[current_Player] += 5;
-            //last_Player = current_Player;
             break;
         default: break;
         }
     }
+    //print who won the game
     printf("\n%s wins with %d points!\n", names[current_Player], points[current_Player]);
 }
