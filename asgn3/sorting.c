3e015c1dc5c2e9eb037df018a585d8f9dbafa825
James Gu <jjgu@ucsc.edu>
2021-10-17 22:37:15 +0000

    added in -h support and -a support to sorting.c

--- asgn3/sorting.c
+++ asgn3/sorting.c
@@ -8,6 +8,7 @@
 #include <inttypes.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 
 #define OPTIONS "aeisqr:n:p:h"
@@ -39,13 +40,29 @@ int main(int argc, char **argv) {
         case 'n':
             size = strtoul(optarg, NULL, 10);
             elements = size;
-            printf("size %u\n", size);
+            //printf("size %u\n", size);
             break;
         case 'p':
             elements = strtoul(optarg, NULL, 10);
-            printf("size %u\n", elements);
+            //printf("size %u\n", elements);
             break;
-        case 'h': s = empty_set(); break;
+        case 'h':
+            s = empty_set();
+            printf("SYNOPSIS\n");
+            printf("   A collection of comparison-based sorting algorithms.\n\n");
+            printf("USAGE\n");
+            printf("   ./sorting [-haeisqn:p:r:] [-n length] [-p elements] [-r seed]\n\n");
+            printf("OPTIONS\n");
+            printf("  -h              display program help and usage.\n");
+            printf("  -a              enable all sorts.\n");
+            printf("  -e              enable Heap Sort.\n");
+            printf("  -i              enable Insertion Sort.\n");
+            printf("  -s              enable Shell Sort.\n");
+            printf("  -q              enable Quick Sort.\n");
+            printf("  -n length       specify number of array elements (default: 100).\n");
+            printf("  -p elements     specify number of elements to print (default: 100).\n");
+            printf("  -r seed         specify random seed (default: 13371453).\n");
+            break;
         }
     }
 
@@ -60,35 +77,17 @@ int main(int argc, char **argv) {
     for (uint32_t i = 0; i < size; i += 1) {
         A[i] = random() & (((long) 1 << (30)) - 1);
     }
-    if (member_set(INSERTION, s)) {
-        insertion_sort(&stats, A, size);
-        printf("Insertion Sort, %u elements, %" PRIu64 " moves, %" PRIu64 " compares\n", size,
-            stats.moves, stats.compares);
-        if (elements > 0) {
-            for (uint32_t i = 0; i < elements; i += 1) {
-                printf("%13" PRIu32, A[i]);
-                if ((i + 1) % 5 == 0) {
-                    printf("\n");
-                }
-            }
-        }
-        reset(&stats);
+    uint32_t B[size];
+    for (uint32_t i = 0; i < size; i += 1) {
+        B[i] = A[i];
     }
-    if (member_set(SHELL, s)) {
-        shell_sort(&stats, A, size);
-        printf("Shell Sort, %u elements, %" PRIu64 " moves, %" PRIu64 " compares\n", size,
-            stats.moves, stats.compares);
-        if (elements > 0) {
-            for (uint32_t i = 0; i < elements; i += 1) {
-                printf("%13" PRIu32, A[i]);
-                if ((i + 1) % 5 == 0) {
-                    printf("\n");
-                }
-            }
-        }
-        reset(&stats);
-    }
+    //uint32_t A[size];
     if (member_set(HEAP, s)) {
+        //memcpy(A, B, size);
+        //uint32_t *A = (uint32_t *) calloc(size, sizeof(uint32_t));
+        //for (uint32_t i = 0; i < size; i += 1) {
+        //	A[i] = random() & (((long) 1 << (30)) - 1);
+        //}
         heap_sort(&stats, A, size);
         printf("Heap Sort, %u elements, %" PRIu64 " moves, %" PRIu64 " compares\n", size,
             stats.moves, stats.compares);
@@ -100,9 +99,67 @@ int main(int argc, char **argv) {
                 }
             }
         }
+        //free(A);
+        //memcpy(A, B, size);
+        for (uint32_t i = 0; i < size; i += 1) {
+            A[i] = B[i];
+        }
+        reset(&stats);
+    }
+    if (member_set(SHELL, s)) {
+        //uint32_t *A = (uint32_t *) calloc(size, sizeof(uint32_t));
+        //for (uint32_t i = 0; i < size; i += 1) {
+        //	A[i] = random() & (((long) 1 << (30)) - 1);
+        //}
+        //memcpy(A, B, size);
+        shell_sort(&stats, A, size);
+        printf("Shell Sort, %u elements, %" PRIu64 " moves, %" PRIu64 " compares\n", size,
+            stats.moves, stats.compares);
+        if (elements > 0) {
+            for (uint32_t i = 0; i < elements; i += 1) {
+                printf("%13" PRIu32, A[i]);
+                if ((i + 1) % 5 == 0) {
+                    printf("\n");
+                }
+            }
+        }
+        //free(A);
+        //memcpy(A, B, size);
+        for (uint32_t i = 0; i < size; i += 1) {
+            A[i] = B[i];
+        }
+        reset(&stats);
+    }
+    if (member_set(INSERTION, s)) {
+        //uint32_t *A = (uint32_t *) calloc(size, sizeof(uint32_t));
+        //for (uint32_t i = 0; i < size; i += 1) {
+        //	A[i] = random() & (((long) 1 << (30)) - 1);
+        //}
+        //memcpy(A, B, size);
+        insertion_sort(&stats, A, size);
+        printf("Insertion Sort, %u elements, %" PRIu64 " moves, %" PRIu64 " compares\n", size,
+            stats.moves, stats.compares);
+        if (elements > 0) {
+            for (uint32_t i = 0; i < elements; i += 1) {
+                printf("%13" PRIu32, A[i]);
+                if ((i + 1) % 5 == 0) {
+                    printf("\n");
+                }
+            }
+        }
+        for (uint32_t i = 0; i < size; i += 1) {
+            A[i] = B[i];
+        }
+        //free(A);
+        //memcpy(A, B, size);
         reset(&stats);
     }
     if (member_set(QUICK, s)) {
+        //uint32_t *A = (uint32_t *) calloc(size, sizeof(uint32_t));
+        //for (uint32_t i = 0; i < size; i += 1) {
+        //	A[i] = random() & (((long) 1 << (30)) - 1);
+        //}
+        //memcpy(A,B, size);
         quick_sort(&stats, A, size);
         printf("Quick Sort, %u elements, %" PRIu64 " moves, %" PRIu64 " compares\n", size,
             stats.moves, stats.compares);
@@ -114,6 +171,9 @@ int main(int argc, char **argv) {
                 }
             }
         }
+        //free(A);
+        //memcpy(A, B, size);
+        reset(&stats);
     }
     free(A);
     return 0;
