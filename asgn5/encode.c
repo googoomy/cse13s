1458ac096016cd3f04cf9bd6096169692d69f3a8
James Gu <jjgu@ucsc.edu>
2021-11-07 21:05:30 +0000

    formatted encode

--- asgn5/encode.c
+++ asgn5/encode.c
@@ -62,7 +62,7 @@ int main(int argc, char **argv) {
         printf("  -v             Print compression statistics.\n");
         printf("  -i infile      Input file to compress.\n");
         printf("  -o outfile     Ouput of compressed data.\n");
-    	return 0;
+        return 0;
     }
     if (i_flag == false) {
         input_file = STDIN_FILENO;
@@ -75,28 +75,28 @@ int main(int argc, char **argv) {
         output_file = open(out, O_WRONLY | O_CREAT);
     }
 
-    uint8_t buf[BLOCK] = {0};
+    uint8_t buf[BLOCK] = { 0 };
     uint32_t count = 0;
     int byte = 0;
-    uint64_t *histogram = (uint64_t *)calloc(ALPHABET, sizeof(uint64_t));
+    uint64_t *histogram = (uint64_t *) calloc(ALPHABET, sizeof(uint64_t));
     //increment element 0 and element 255 by 1 so at the very minimum the histogram will have two elements present
     histogram[0] += 1;
     histogram[255] += 1;
     //construct the histogram while also keeping track of how many unique characters there are
-    while((byte = read(input_file, buf, BLOCK)) > 0){
-    	for(int i = 0; i < byte; i += 1){
-		histogram[buf[i]] += 1;
-		if(histogram[buf[i]] == 1){
-			count += 1;
-		}
-	}
+    while ((byte = read(input_file, buf, BLOCK)) > 0) {
+        for (int i = 0; i < byte; i += 1) {
+            histogram[buf[i]] += 1;
+            if (histogram[buf[i]] == 1) {
+                count += 1;
+            }
+        }
     }
-    
+
     //construct the huffman tree with the histogram
     Node *huffman_tree_root = build_tree(histogram);
     //construct a code table by traversing the huffman tree
     Code table[ALPHABET];
-    build_codes(huffman_tree_root, table); 
+    build_codes(huffman_tree_root, table);
     //construct a header
     Header header;
     header.magic = MAGIC;
@@ -115,12 +115,12 @@ int main(int argc, char **argv) {
     //any remaining buffered codes with flush_codes()
     //go back to the beginning of the input_file:
     lseek(input_file, 0, SEEK_SET);
-    uint8_t buff[BLOCK] = {0};
+    uint8_t buff[BLOCK] = { 0 };
     uint32_t bytes = 0;
-    while((bytes = read_bytes(input_file, buff, BLOCK)) > 0){
-    	for(uint32_t i = 0; i < bytes; i += 1){
-		write_code(output_file, &table[buff[i]]);
-	}
+    while ((bytes = read_bytes(input_file, buff, BLOCK)) > 0) {
+        for (uint32_t i = 0; i < bytes; i += 1) {
+            write_code(output_file, &table[buff[i]]);
+        }
     }
     flush_codes(output_file);
     free(histogram);
@@ -128,15 +128,4 @@ int main(int argc, char **argv) {
     //close infile and outfile
     close(output_file);
     close(input_file);
-
-
-
-	
-
-
-
-
-
-
-
 }
